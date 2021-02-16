c9c4567edd51d7ed7061d689df2c2aaf846194af
manbalboy <manbalboy@hanmail.net>
2021-02-16 21:01:16 +0900

    라우터 구성중

---
+++ vue-news-manbalboy/src/routers/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+
+
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+
+    routes: [
+      {
+        path: '/',
+        redirect: '/news' 
+      },
+
+      {
+        path: '/news',
+        component: NewsView,
+      },
+
+      {
+        path: '/ask',
+        component: AskView,
+      },
+
+      {
+        path: '/jobs',
+        component: JobsView,
+      },
+
+    //   {
+    //     path: '/item',
+    //     component: ItemView,
+    //   },
+
+    //   {
+    //     path: '/user',
+    //     component: UserView,
+    //   }
+    ]
+  })
