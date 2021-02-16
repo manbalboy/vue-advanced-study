b31c1b1fd5811b35aad5ffffdf2b61d47a479e6d
manbalboy <manbalboy@hanmail.net>
2021-02-16 21:29:32 +0900

    axios 테스트

--- vue-news-manbalboy/src/components/ToolBar.vue
+++ vue-news-manbalboy/src/components/ToolBar.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="header">
+        <img src="../assets/logo.svg" alt="logo" class="logo">
         <router-link to="/news">News</router-link> |
         <router-link to="/ask">Ask</router-link>  |
         <router-link to="/jobs">Jobs</router-link> 
@@ -15,9 +16,10 @@
 <style scoped>
     .header {
         background-color: #42b883;
-        display: flex;
-        padding: 8px;
         color: white;
+        padding: 8px 8px 8px 25px;
+        display: flex;
+        align-items: center;
     }
 
     .header .router-link-exact-active {
@@ -26,5 +28,20 @@
 
     .header a {
         color: white;
+    }
+    
+    .fixed {
+        position: fixed;
+        width: 100%;
+        z-index: 999;
+    }
+
+    .logo {
+        width: 30px;
+        margin-right: 18px;
+    }
+
+    a {
+        margin: 0 5px;
     }
 </style>
--- vue-news-manbalboy/src/views/NewsView.vue
+++ vue-news-manbalboy/src/views/NewsView.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script>
-    export default {
+    import axios from 'axios';
 
+    export default {
+        created() {
+            axios.get('https://api.hnpwa.com/v0/news/1.json')
+                .then(function (response) {
+                    console.log('gg >' , response);
+                })
+                .catch(function (){})
+        }
     }
 </script>
 
