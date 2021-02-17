bafcfb17fdfb2363053999c060370e16ef97e66c
manbalboy <manbalboy@hanmail.net>
2021-02-17 17:34:42 +0900

    vue transition

--- vue-news-manbalboy/src/App.vue
+++ vue-news-manbalboy/src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="app">
         <tool-bar></tool-bar>
-        <router-view></router-view>
+        <transition name="page">
+            <router-view></router-view>
+        </transition>
         <footer-comp></footer-comp>
     </div>
 </template>
@@ -25,5 +27,13 @@
     body {
         padding: 0;
         margin: 0;
+    }
+
+    .page-enter-active, .page-leave-active {
+        transition: opacity .5s;
+    }
+
+    .page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+        opacity: 0;
     }
 </style>
