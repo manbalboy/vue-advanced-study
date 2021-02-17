fe53df139594ef50e0564e29a24251f06391d775
manbalboy <manbalboy@hanmail.net>
2021-02-17 23:52:19 +0900

    props 로 data 연동

---
+++ vue-news-manbalboy/src/components/UserProfile.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="user-container">
+        <div>
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+            <div> {{ info.id }} </div>
+            <div class="time">
+                {{ info.created }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props : {
+            info : Object
+        }
+        // computed : {
+        //     userInfo(){ 
+        //         return this.$store.state.user;
+        //     }
+        // },
+    }
+</script>
+
+<style>
+    .user-container {
+        display: flex;
+        align-items: center;
+        padding: 0.5rem;
+    }
+
+    .fa-user {
+        font-size: 2.5rem;
+    }
+
+    .user-description{
+        padding-left: 8px;
+    }
+
+    .time {
+        font-size: 0.7rem;
+    }
+</style>
