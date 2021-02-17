94152983a6b96b68e575a602df819bbb95c6b128
manbalboy <manbalboy@hanmail.net>
2021-02-17 19:52:12 +0900

    주석정리

--- vue-news-manbalboy/src/views/JobsView.vue
+++ vue-news-manbalboy/src/views/JobsView.vue
@@ -1,26 +1,6 @@
 <template>
     <div> 
         <list-item></list-item>
-        <!-- <ul class="news-list">
-            <li class="post" v-for="item in this.$store.state.jobs" v-bind:key="item.id">
-               
-                <div class="points">
-                    {{ item.points || 0 }}
-                </div>
-
-                <div>
-                    <p class="news-tilte">
-                        <a v-bind:href="item.url"> {{item.title}} </a>
-                    </p>
-                     <small class="link-text">
-                        {{item.time_ago}} by 
-                        <a v-bind:href="item.url">
-                            {{item.domain}}
-                        </a>
-                    </small>
-                </div>
-            </li>
-        </ul> -->
     </div>
 </template>
 
