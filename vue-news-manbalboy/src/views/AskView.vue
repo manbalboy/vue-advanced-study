458779a3ca03d2e19465cbc64930e5982c072487
manbalboy <manbalboy@hanmail.net>
2021-02-17 10:24:45 +0900

    ask jobs 엑시오스 레이아웃 구현

--- vue-news-manbalboy/src/views/AskView.vue
+++ vue-news-manbalboy/src/views/AskView.vue
@@ -1,11 +1,31 @@
 <template>
     <div>
-        ask
+        <div v-for="item in ask" v-bind:key="item">
+            {{item.title}}
+        </div>
     </div>
 </template>
 
 <script>
+    import { fetchAskList } from '../api/index.js';
     export default {
+        data() {
+            return {
+                ask : []
+            }
+        },
+        // 라이프사이클 훅 mounted, beforeMount created ....
+        created() {
+            let vm = this;
+            fetchAskList()
+            .then(respons => {
+                vm.ask = respons.data;
+            })
+            .catch(err => {
+                console.log('str >>>>> ',  err);
+            });
+        }
+
 
     }
 </script>
--- vue-news-manbalboy/src/views/JobsView.vue
+++ vue-news-manbalboy/src/views/JobsView.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        jobs
+        <div v-for="item in jobs" v-bind:key="item">
+            {{item.id}}
+        </div>
     </div>
 </template>
 
 <script>
+    import { fetchJobsList } from '../api/index.js';
     export default {
+        data(){
+            return  {
+                jobs : []
+            }
+        },
 
+        created() {
+            let vm = this;
+            fetchJobsList() 
+            . then (response => {
+                vm.jobs = response.data;
+            }) 
+            . catch(err => {
+                console.log('str  >>>>> ',  err );
+            })
+        },
     }
 </script>
 
