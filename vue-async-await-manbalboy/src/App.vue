71884a698408e08fed62b357f998980bb3c81587
manbalboy <manbalboy@hanmail.net>
2021-02-19 13:19:48 +0900

    async await 학습용 Vue 프로젝트 생성

--- vue-async-await-manbalboy/src/App.vue
+++ vue-async-await-manbalboy/src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <div>
-    <button @click="loginUser">login</button>
-    <h1>List</h1>
-  </div>
+    <div>
+        <button @click="loginUser">login</button>
+        <h1>List</h1>
+        <ul>
+            <li v-for="(item,index) in items" v-bind:key="index">
+                {{item}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import axios from 'axios';
+    import axios from 'axios';
 
-export default {
-  data() {
-    return {
-      items: [],
+    export default {
+        data() {
+            return {
+                items: [],
+            }
+        },
+        methods: {
+            loginUser() {
+                axios.get('https://jsonplaceholder.typicode.com/users/1')
+                    .then(response => {
+                            console.log(response);
+                            
+                            if(response.data.id === 1) {
+                                axios.get('https://jsonplaceholder.typicode.com/users/1')
+                                    .then(response => {
+                                            this.items = response.data;
+                                        })
+                                    .catch(error => console.log(error));
+                            }
+
+                        })
+                    .catch(error => console.log(error));
+            },
+        },
     }
-  },
-  methods: {
-    loginUser() {
-      axios.get('https://jsonplaceholder.typicode.com/users/1')
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-    },
-  },
-}
 
 </script>
