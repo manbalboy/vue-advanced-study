8a3d6b49e874b31a8cd17d8531e81950d94ca221
manbalboy <manbalboy@hanmail.net>
2021-02-19 17:07:41 +0900

    라인차트 등록 - 충돌발생시킴

--- vue-lib-chart-study-manbalboy/src/App.vue
+++ vue-lib-chart-study-manbalboy/src/App.vue
@@ -3,6 +3,7 @@
         <article>
             <h1>Chart JS</h1>
             <bar-chart></bar-chart>
+            <line-chart></line-chart>
         </article>
     </div>
 </template>
@@ -12,10 +13,12 @@
     // 2. 설치된 라이브러리를 import로 App.vue 에서 로딩
     // 3. mounted() 라이프 사이클 훅에서 차트를그림
     import BarChart from './components/BarChart.vue';
+    import LineChart from './components/LineChart.vue';
     export default {
         //컴포넌트 속성 && 인스턴스 속성
         components : {
-            BarChart
+            BarChart,
+            LineChart
         }
        
     }
---
+++ vue-lib-chart-study-manbalboy/src/components/LineChart.vue
@@ -0,0 +1,34 @@
+<template>
+    <canvas id="myChart"></canvas>
+</template>
+
+<script>
+    import Chart from 'chart.js';
+    export default {
+        mounted() {
+            var ctx = document.getElementById('myChart').getContext('2d');
+            new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'line',
+
+                // The data for our dataset
+                data: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [{
+                        label: 'My First dataset',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [0, 10, 5, 2, 20, 30, 45]
+                    }]
+                },
+
+                // Configuration options go here
+                options: {}
+            });
+        },
+    }
+</script>
+
+<style>
+
+</style>
