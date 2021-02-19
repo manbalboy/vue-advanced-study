686ae03a1b46d1fbbd5733c4c2036d44b97398b2
manbalboy <manbalboy@hanmail.net>
2021-02-19 13:29:18 +0900

    async await 적용

--- vue-async-await-manbalboy/src/App.vue
+++ vue-async-await-manbalboy/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button @click="loginUser">login</button>
+        <button @click="loginUser1">login</button>
         <h1>List</h1>
         <ul>
             <li v-for="(item,index) in items" v-bind:key="index">
@@ -23,11 +23,12 @@
             loginUser() {
                 axios.get('https://jsonplaceholder.typicode.com/users/1')
                     .then(response => {
-                            console.log(response);
+                            console.log('사용자 인증');
                             
                             if(response.data.id === 1) {
                                 axios.get('https://jsonplaceholder.typicode.com/users/1')
                                     .then(response => {
+                                            console.log('DATA 확인');
                                             this.items = response.data;
                                         })
                                     .catch(error => console.log(error));
@@ -36,6 +37,14 @@
                         })
                     .catch(error => console.log(error));
             },
+
+            async loginUser1 () {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+                if(response.data.id === 1) {
+                    const list = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+                    this.items = list.data;
+                }
+            }
         },
     }
 
--- vue-async-await-manbalboy/src/utils/handler.js
+++ vue-async-await-manbalboy/src/utils/handler.js
@@ -1,3 +1,3 @@
-export function handleException(status) {
+export function handleException() {
   // ..
 }
